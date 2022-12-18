44f6bf58bc86173a457d4261298c2bfba0c74416
Garrett Winter <Garrett_Winter@hotmail.com>
2022-12-18 02:06:47 -0600

    TIghten up intial combat, started basic ui.

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ playerHealthDisplay.textContent = playerHealth;
 let playerLevel = 1;
 let playerArmorClass = 2;
 
-let playerAttackDMG = 15;
+let playerAttackDMG = 10;
 let playerCritChance = 5;
 
 let playerHitRoll;
@@ -31,49 +31,101 @@ encHealthDisplay.textContent = encHealth;
 let encLevel = 1;
 let encArmorClass = 2;
 
-let encAttackDMG = 15;
+let encAttackDMG = 10;
 let encCritChance = 5;
 
-let enchitRoll;
-let encdamage; 
+let encHitRoll;
+let encDamage; 
 
 
 attackBtn.addEventListener("click", playerAttack);
 
 function playerAttack (){
-    
-        // does it hit
+    console.log("playerAttack Ran");
+            // does it hit
     playerHitRoll = Math.floor(Math.random()*100);
-    console.log ("Rolled a "+playerHitRoll);
+    console.log(playerHitRoll);
         // for how much
     if (playerHitRoll >  (100 - playerCritChance)) {
         playerDamage = Math.floor(Math.random()*(playerAttackDMG*2));
-        console.log("CRITICAL HIT, hit for "+playerDamage);
+        // console.log("TEMP - Player Crit");
         encMitigation(playerDamage);
         return;
     }
     if (playerHitRoll >  65) {
         playerDamage = Math.floor(Math.random()*playerAttackDMG);
-        console.log("HIT, hit for "+playerDamage);
+        // console.log("TEMP - Player hit");
         encMitigation(playerDamage);
         return;
+    } if (playerHitRoll <= 64) {
+        console.log("Player has missed their attack.");
+        encAttack()
+    };
     }
-}
 
-function encMitigation(){
-    encEvadeChance = Math.floor(Math.random()*100);
-    if (encEvadeChance > (100-encAgi)){
-        playerDamage = 0;
+function encMitigation() {
+    encEvadeChance = Math.floor(Math.random() * 100);
+    console.log(encEvadeChance);
+    if (encEvadeChance > (100 - encAgi)) {
+        playerDamage = 0; //Clearing Value
+        console.log("Players attacked has been dodged the Encounter.");
+        encAttack()
         return;
     };
     playerDamage = (playerDamage - encArmorClass);
-        if (playerDamage <= 0) {
-            playerDamage = 0;
-        console.log("damage neturalized");
+    if (playerDamage <= 0) {
+        playerDamage = 0;
+        console.log("Players damage neturalized by the encounters armor class");
+        encHealth = encHealth - playerDamage;
+        encHealthDisplay.textContent = encHealth;
+        encAttack();
+        return;
     }
     encHealth = encHealth - playerDamage;
     encHealthDisplay.textContent = encHealth;
-    console.log("You have been hit for " + playerDamage + ", your health is now " + encHealth);
+    console.log("Player has hit the encounter for " + playerDamage + ", their health is now " + encHealth);
+    encAttack();
+}
+
+function encAttack (){
+    // console.log("encAttack Ran");
+        // does it hit
+    encHitRoll = Math.floor(Math.random()*100);
+    // console.log(encHitRoll);
+        // for how much
+    if (encHitRoll >  (100 - encCritChance)) {
+        encDamage = Math.floor(Math.random()*(encAttackDMG*2));
+        // console.log("TEMP - Crit;");
+        playerMitigation(encDamage);
+        return;
+    }
+    if (encHitRoll >=  65) {
+        encDamage = Math.floor(Math.random()*encAttackDMG);
+        // console.log("TEMP - Hit");
+        playerMitigation(encDamage);
+        return;
+    }
+    console.log("Encounter has missed their attack.");
+}
+
+function playerMitigation(){
+    playerEvadeChance = Math.floor(Math.random()*100);
+    if (playerEvadeChance > (100-playerAgi)){
+        encDamage = 0;
+        console.log("Player has dodged and avoided the attack.")
+        return;
+    };
+    encDamage = (encDamage - playerArmorClass);
+        if (encDamage <= 0) {
+            encDamage = 0;
+        console.log("Encounters damage was absorbed by the players armor.");
+        playerHealth = playerHealth - encDamage;
+        playerHealthDisplay.textContent = playerHealth;
+        return;
+    }
+    playerHealth = playerHealth - encDamage;
+    playerHealthDisplay.textContent = playerHealth;
+    console.log("Encounter has hit Player hit for " + encDamage + ", their health is now " + playerHealth);
 }
 
 /* Pseduo Coding
